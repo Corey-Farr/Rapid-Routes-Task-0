3b57e292ca201116bc111a7214d9224d01a2d038
Corey-Farr <corey.p.farr@gmail.com>
2025-02-04 13:58:20 -0700

    Cleared up code

--- index.ts
+++ index.ts
@@ -1,14 +1,7 @@
 import dotenv from "dotenv"
-import TRANSCRIPTION from "./openai-functions/openai-transcribe";
 import RESPONSE from "./openai-functions/openai-prompt";
 
 dotenv.config();
-
-
-// FOR DEMONSTRATION. DELETE AFTER
-console.log(`FOR DEMONSTRATION. DELETE AFTER\nTranscription:\n${TRANSCRIPTION}\nFOR DEMONSTRATION. DELETE AFTER\n\n`);
-// FOR DEMONSTRATION. DELETE AFTER
-
 
 // Log AI's response
 console.log(`Response:\n${RESPONSE}`);
