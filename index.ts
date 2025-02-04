06e5e5ba29b028be73ab6bd6c08cb7915201fc14
Corey-Farr <corey.p.farr@gmail.com>
2025-02-04 14:14:27 -0700

    Cleaned up code

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv"
 import RESPONSE from "./openai-functions/openai-prompt";
 
-dotenv.config();
+// import dotenv from "dotenv"
+// dotenv.config();
 
 // Log hardcoded sample response to hardcoded sample transcription
 console.log(`Response:\n${RESPONSE}`);
